c26287ddc5de4d49334fee51c0d80ef74872c5eb
Alexis Kreager <alexisdkreager@gmail.com>
2020-06-11 10:39:12 -0500

    add image, add list of todo's

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
     for (const element of listItems) {
         element.style.backgroundColor = "yellow";
     }
-
     //Other types of loops we could have used
     /*
     for (let i = 0; i < listItems.length; i++) {
@@ -24,4 +23,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     })
     */
+
+    //create an image tag, set src, give id 'greeting' to div
+    let newImg = document.createElement('img');
+    newImg.setAttribute('src', 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif')
+    document.getElementById('greeting').appendChild(newImg);
+
+    //add to do items in unordered list
+    let ulItems = document.createElement('ul');
+    ulItems.setAttribute('class', 'todo-items');
+    document.querySelector('body').appendChild(ulItems)
+
+    let toDoArray = ['make coffee','eat donut','change diapers','drive to work'];
+    for (let todo of toDoArray) {
+        let newItem = document.createElement('li');
+        newItem.innerText = todo;
+        document.querySelector('.todo-items').appendChild(newItem);
+    }
+
 })
