d9220bc6907959013600cd37c2e3aa25246292d3
Alexis Kreager <alexisdkreager@gmail.com>
2020-06-11 10:02:18 -0500

    greeting and li background color

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("Javascript is alive!");
+
+document.addEventListener('DOMContentLoaded', function () {
+    //change Hello, There to Hello, World
+    document.getElementById('greeting').innerText = "Hello World!";
+
+    //change background color of li elements to yellow
+    let listItems = document.getElementById('essentials').children[0].children;
+    for (const element of listItems) {
+        element.style.backgroundColor = "yellow";
+    }
+
+    //Other types of loops we could have used
+    /*
+    for (let i = 0; i < listItems.length; i++) {
+
+    }
+    for(const key in object) {
+       if (object.hasOwnProperty(k)) {
+           const element = object[k];
+       }
+    }
+    listItems.forEach(element => {
+
+    })
+    */
+})
